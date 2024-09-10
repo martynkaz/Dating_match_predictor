55bd023d0e0f1c4451da72f5aab3a1959c080286
martynkaz <martyna.zglinska9@gmail.com>
2024-09-10 22:24:22 +0100

    Initialise the app

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 import pandas
+import streamlit as st
+
+st.title("💗 Dating Match Predictor 💗")
+
+st.sidebar.header("Please fill out the information below")
+st.sidebar.selectbox("Pick your gender", ["Male","Female"])
+st.sidebar.selectbox("Did you purchase a VIP account", ["No","Yes"])
+st.sidebar.slider("Please specify your income", 15000,30000)
+st.sidebar.selectbox("How many children do you have", [0,1,2,3,4,5,6])
+st.sidebar.slider("How would you rate your attractiveness",0,10)
+st.sidebar.button("Submit")
+
