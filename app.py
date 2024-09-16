72b67c6278f0a0c74b64a81649e238931c58a519
martynkaz <martyna.zglinska9@gmail.com>
2024-09-16 22:05:59 +0100

    Updates

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import pandas
 import streamlit as st
 
+@st.cache(suppress_st_warning=True)
+def get_fvalue(val):    
+    feature_dict = {"No":1,"Yes":2}    
+    for key,value in feature_dict.items():       
+         if val == key:           
+             return value
+         
+def get_value(val,my_dict):    
+    for key,value in my_dict.items():        
+        if val == key:            
+            return value
+
+app_mode = st.sidebar.selectbox('Select Page',['Home','Prediction']) #two pages
+
 st.title("💗 Dating Match Predictor 💗")
 
 st.sidebar.header("Please fill out the information below")
